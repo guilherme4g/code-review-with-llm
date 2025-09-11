c7adce1d22480b12f8ae0fe12cadc7894fa39e00
F5549019 Jose Guilherme Nascimento Vieira Da Silva <guilherme.vieira@bb.com.br>
2025-09-11 15:13:31 -0300

    feat: create routes

---
+++ src/gitlab-reviewer/gitlab-reviewer.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post, Req } from '@nestjs/common';
+import { GitlabReviewerService } from "./gitlab-reviewer.service";
+
+@Controller('gitlab-reviewer')
+export class GitlabReviewerController {
+
+  constructor(private readonly gitlabReviewerService: GitlabReviewerService) { }
+  
+  @Post()
+  reviewMergeRequest(@Body() Body: any): string {
+    return this.gitlabReviewerService.processMergeRequest();
+  }
+}
