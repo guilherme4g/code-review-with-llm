faa433455b82b677a060eee5ae9c50104ff7b105
Guilherme <jgnvs6@gmail.com>
2025-10-06 22:20:58 -0300

    feat: implement github reviewer

--- src/github-reviewer/github-reviewer.controller.ts
+++ src/github-reviewer/github-reviewer.controller.ts
@@ -1,4 +1,7 @@
-import { Controller, Post, Body } from '@nestjs/common';
+//import fs from "fs";
+import express from 'express';
+import { BadRequestException, Controller, HttpCode, HttpStatus, Post, Req } from '@nestjs/common';
+import { PullRequestSummary } from "./pull-request-summary";
 import { GithubReviewerService } from "./github-reviewer.service";
 
 @Controller('github-reviewer')
@@ -7,8 +10,36 @@ export class GithubReviewerController {
   constructor(private readonly githubReviewerService: GithubReviewerService) { }
 
   @Post()
-  reviewPullRequest(@Body() body: object): string {
-    console.log(`body:${JSON.stringify(body)}`);
-    return this.githubReviewerService.processPullRequest();
+  @HttpCode(HttpStatus.OK)
+  async reviewPullRequest(@Req() request: express.Request): Promise<void> {
+    
+    if (!this.validateRequest(request.body)) {
+      throw new BadRequestException("Not valid github request");
+    }
+    
+    // const filename = `${__dirname}/PR-${Date.now()}`;
+    // console.log(`save at: ${filename}`)
+    //  fs.writeFileSync(`${filename}`,  request.body);
+
+    const pullRequestSummary : PullRequestSummary = {
+      repo: request.body.repository.name,
+      owner: request.body.repository.owner.login,
+      pull_number: request.body.pull_request.number,
+      action: request.body.action,
+      state: request.body.pull_request.state,
+      diff_url: request.body.pull_request.diff_url
+    }
+
+    const result = await this.githubReviewerService.processPullRequest(pullRequestSummary);
+
+    if (result instanceof Error) {
+      throw new BadRequestException(result.message);
+    } else {
+      return;
+    }
+  }
+
+  validateRequest (body: any): boolean {
+    return body && body.action && body.pull_request && body.pull_request.state && body.pull_request.diff_url;
   }
 }
---
+++ src/github-reviewer/pull-request-summary.ts
@@ -0,0 +1,8 @@
+export class PullRequestSummary {
+    repo: string;
+    owner: string;
+    pull_number: number;
+    action: string;
+    state: string;
+    diff_url: string;
+}
